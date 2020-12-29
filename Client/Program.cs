92f5b8579e65816ee978b2e196e11f7f08e74657
Matthias Hofmarcher <hofmarchermatthias@gmail.com>
2020-12-29 12:45:40 +0100

    Demo finished

---
+++ Client/Program.cs
@@ -0,0 +1,85 @@
+﻿using IdentityModel.OidcClient;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClientWithBrowser
+{
+    public class Program
+    {
+        const string Authority = "https://localhost:5001";
+        const string Api = "https://localhost:443/api";
+
+        static OidcClient _oidcClient;
+        static HttpClient _apiClient = new HttpClient { BaseAddress = new Uri(Api) };
+
+        public static void Main() => MainAsync().GetAwaiter().GetResult();
+
+        public static async Task MainAsync()
+        {
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("|  Sign in with OIDC    |");
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("");
+            Console.WriteLine("Next step: Login");
+            Console.ReadLine();
+
+            await Login();
+        }
+
+        private static async Task Login()
+        {
+            var port = 6655;
+            var browser = new SystemBrowser(port: port);
+            string redirectUri = string.Format($"http://localhost:{port}/");
+
+            var options = new OidcClientOptions
+            {
+                Authority = Authority,
+                ClientId = "interactive.public",
+                RedirectUri = redirectUri,
+                Scope = "openid profile",
+                FilterClaims = false,
+                Browser = browser,
+                Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode,
+                ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect
+            };
+
+            _oidcClient = new OidcClient(options);
+            var result = await _oidcClient.LoginAsync(new LoginRequest());
+
+            Console.WriteLine("\n\nClaims:");
+            foreach (var claim in result.User.Claims)
+            {
+                Console.WriteLine("{0}: {1}", claim.Type, claim.Value);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"id token: {result.IdentityToken}");
+            Console.WriteLine($"access token:   {result.AccessToken}");
+            Console.WriteLine($"refresh token:  {result.RefreshToken}");
+
+            Console.WriteLine("Next step: Call API");
+            var key = Console.ReadLine();
+            await CallApi(result.AccessToken);
+        }
+
+        private static async Task CallApi(string currentAccessToken)
+        {
+            _apiClient.SetBearerToken(currentAccessToken);
+            var response = await _apiClient.GetAsync("WeatherForecast");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = JArray.Parse(await response.Content.ReadAsStringAsync());
+                Console.WriteLine("\n\n");
+                Console.WriteLine(json);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.ReasonPhrase}");
+            }
+        }
+    }
+}
---
+++ is4/Config.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace is4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "scope1" }
+                },
+
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope2" }
+                },
+                new Client
+                {
+                    ClientId = "interactive.public",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RedirectUris = { "http://localhost:6655/" },
+                    AllowedCorsOrigins = {"http://localhost:6655"},
+                    AllowOfflineAccess = true,
+                    RequirePkce = true,
+                    AllowedScopes = { "openid", "profile" },
+                    RequireConsent = true,
+                },
+            };
+    }
+}
